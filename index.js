663a3de5f2f718bf397cb7f3f5d0ae63231dce1d
Izabella Novikova <belka215@gmail.com>
2024-06-09 19:18:55 +0200

    another commit

--- index.js
+++ index.js
@@ -1,40 +1,81 @@
-const newName = "Jon";
-newName.filter((item) => {});
-
-let a = "fewfw";
-a = 5;
-
-// const array = new Array(1000).fill(null).map((_, i) => ++i);
-// console.log(array);
-
-const testList = [1, 3, 55, 851.3];
-// const name = "Jon";
-let age = 30;
-let city = "Minsk";
-
-const getStringsList = (array) => {
-  array.toLowerCase();
-};
-
-const newTestList = getStringsList(testList, 15, "Jon", true);
-console.log(newTestList);
-
-// document.body.addEventListener("click", () => {
-//   const newTestList = getStringsList(testList);
-//   console.log(newTestList);
-// });
-
-const array = new Array(1000).fill(null).map((_, i) => ++i);
-
-const PERSENT = 0.14;
-
-const newArray = array.map((item) => {
-  // console.log(typeof PERSENT); // number
-  const sum = PERSENT + item; // 1 на первой итерации
-
-  return sum;
+(function () {
+    // const array = new Array(1000).fill(null).map((_, i) => ++i);
+    // // console.log(array);
+    var users = [
+        {
+            id: 7,
+            email: "michael.lawson@reqres.in",
+            first_name: "Michael",
+            last_name: "Lawson",
+            age: 23,
+        },
+        {
+            id: 8,
+            email: "lindsay.ferguson@reqres.in",
+            first_name: "Lindsay",
+            last_name: "Ferguson",
+            avatar: "https://reqres.in/img/faces/8-image.jpg",
+            age: 20,
+        },
+    ];
+    2;
+    //   const euroCur = 'euro';
+    var euroCur = 'euro';
+    //   type OriductsType
+    var products = [
+        {
+            id: 1,
+            name: "Burger Premium",
+            price: 6,
+            currency: euroCur,
+            ingredients: ["flour", "beef", "salad", "cheese", "sauce"],
+            type: "burger",
+            available: true,
+        },
+        {
+            id: 2,
+            name: "Burger Lite",
+            price: 2,
+            currency: "usa",
+            ingredients: ["flour", "beef", "cheese", "sauce", "cucumber"],
+            type: "burger",
+            available: true,
+        },
+        {
+            id: 3,
+            name: "Colla Lite",
+            price: 17,
+            currency: "ru",
+            ingredients: [],
+            type: "drink",
+            available: true,
+        },
+    ];
+    3;
+    var cat = {
+        voice: 'miaw',
+    };
+    var dog = {
+        goodBoy: true,
+    };
+    // const isCat = (obj: ICat | IDog): obj is ICat => {
+    //     return 'voice' in obj;
+    // };
+    // const isString = (value: unknown): value is string => {
+    //     return typeof value === 'string';
+    // }
+    // const testFun = (obj: ICat | IDog) => {
+    //     // if ('voice' in obj) {
+    //     //     console.log(obj.voice);
+    //     // } else {
+    //     //     console.log(obj.goodBoy);
+    //     // }
+    //     if (isCat(obj)) {
+    //         console.log(obj.voice);
+    //     } else {
+    //         console.log(obj.goodBoy);
+    //     }
+    // }
+    // testFun(cat);
+    // testFun(dog);
 });
-
-// const a = true || false;
-
-// const b = true && false;
